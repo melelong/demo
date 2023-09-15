ee117ae00f86fd9495742404f1cb87743177a056
mele <1137042726@qq.com>
2023-09-15 18:48:21 +0800

    防调试

--- 防调试/js/Check.js
+++ 防调试/js/Check.js
@@ -2,13 +2,13 @@ export default class Check {
   time
   constructor({ WorkerUrl, time = 0 }) {
     this.time = time
-    this.#_checkTool()
-    this.#_checkDebugger(WorkerUrl)
+    this._checkTool()
+    this._checkDebugger(WorkerUrl)
   }
   /**
    * 检测打开开发者工具
    */
-  #_checkTool() {
+  _checkTool() {
     const checkWidth = 0.7959606421543242
     const checkHeight = 0.691470054446461
     const checkWidthSize = 0.95
@@ -20,33 +20,43 @@ export default class Check {
       return window.innerHeight / window.outerHeight
     }
     let open = (getWidthSize() <= checkWidthSize && getWidthSize() !== checkWidth) || (getHeightSize() <= checkHeightSize && getHeightSize() !== checkHeight)
-    open && this.#_checkClose()
-    window.addEventListener('resize', () => {
+    open && this._checkClose()
+    const d = this._debounce(() => {
       open = (getWidthSize() <= checkWidthSize && getWidthSize() !== checkWidth) || (getHeightSize() <= checkHeightSize && getHeightSize() !== checkHeight)
-      open && this.#_checkClose()
-    })
+      open && this._checkClose()
+    }, 500)
+    window.addEventListener('resize', d)
   }
   /**
    * 检测debugger
    */
-  #_checkDebugger(WorkerUrl) {
+  _checkDebugger(WorkerUrl) {
     const worker = new Worker(WorkerUrl)
     worker.onmessage = async ({ data }) => {
-      this.#_checkClose()
+      this._checkClose()
     }
     worker.postMessage('不要打印')
   }
-  async #_checkClose() {
+  async _checkClose() {
     const span = document.querySelector('span')
     span.innerText = `检测到调试${this.time}毫秒后关闭`
-    this.time !== 0 && await this.#_sleep(this.time)
-    this.#_closeWindow()
+    this.time !== 0 && await this._sleep(this.time)
+    this._closeWindow()
+  }
+  _debounce(func, delay) {
+    let timeoutId;
+    return function (...args) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
   }
 
   /**
    * 关闭当前标签页
    */
-  #_closeWindow() {
+  _closeWindow() {
     window.open('https://www.baidu.com', '_self')
     window.close()
   }
@@ -55,7 +65,7 @@ export default class Check {
    * @param {*} time 
    * @returns 
    */
-  #_sleep(time = 1000) {
+  _sleep(time = 1000) {
     return new Promise((resolve, reject) => {
       setTimeout(resolve, time)
     })
--- 防调试/js/myConsole.js
+++ 防调试/js/myConsole.js
@@ -3,18 +3,18 @@
  */
 export default class MyConsole {
   // 定时器id
-  #_clearId = null;
+  _clearId = null;
   // 定时器时间
   clearTimer
   constructor(clearTimer = 1000) {
     this.clearTimer = clearTimer
-    this.#_cloneConsole()
-    this.#_startClear()
+    this._cloneConsole()
+    this._startClear()
   }
   /**
    * 克隆console
    */
-  #_cloneConsole() {
+  _cloneConsole() {
     for (let key in console) {
       const value = console[key]
       if (value instanceof Function && key !== 'clear') {
@@ -30,14 +30,14 @@ export default class MyConsole {
   /**
    * 开启清空
    */
-  #_startClear() {
-    this.#_clearId = setInterval(this.clear, this.clearTimer)
+  _startClear() {
+    this._clearId = setInterval(this.clear, this.clearTimer)
   }
   /**
    * 停止清空
    */
   stopClear() {
-    clearInterval(this.#_clearId)
-    this.#_clearId = null
+    clearInterval(this._clearId)
+    this._clearId = null
   }
 }
